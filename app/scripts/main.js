20971208c3a3bad2c2aff5d0509ccd2ddef8f6f8
Moises Morillo <kemdmq@gmail.com>
2016-04-30 14:01:36 -0430

    Fix watch and add angular example

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,5 @@
 'use strict';
 (function() {
   console.log('Welcome to Frontend Development Kit');
+  var app = angular.module('fdk', []);
 })();
--- tasks/watch.js
+++ tasks/watch.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
   browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
-  gulp.watch('./app/**/*.scss', ['styles']);
+  gulp.watch('./app/styles/*.scss', ['styles']);
   gulp.watch('./app/**/*.jade', ['template']);
+  gulp.watch('./app/scripts/*.js', ['scripts'])
 });
