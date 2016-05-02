06a70d3873d12019ef5e0b63ac6ca682b02f21ab
Moises Morillo <kemdmq@gmail.com>
2016-05-01 22:08:27 -0400

    Add gulp-eslint

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,8 +1,17 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  $ = require('gulp-load-plugins')();
 
-gulp.task('scripts', function() {
+
+gulp.task('scripts', ['eslint'], function() {
   return gulp.src('./app/**/*.js')
     .pipe(gulp.dest('.tmp'));
+});
+
+gulp.task('eslint', function() {
+  return gulp.src('./app/scripts/*.js')
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failAfterError());
 });
